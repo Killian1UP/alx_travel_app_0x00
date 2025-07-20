b2428ab6ccf469fcef95270a71d21e5e86319cdf
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-07-20 15:16:01 +0200

    Modified the file.

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'listing.apps.ListingConfig',
+    'listings.apps.ListingsConfig',
     'rest_framework',
     'corsheaders',
     'drf_yasg',
